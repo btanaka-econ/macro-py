c4bc51496a108966e480fa4f65aab3d84d500958
Anonymous <anon@example.com>
2025-06-10 14:15:28 +0900

    Add loop over countries and λ values for HP-filter trend decomposition

--- project_1_business_cycle.py
+++ project_1_business_cycle.py
@@ -8,25 +8,32 @@ import numpy as np
 # set the start and end dates for the data
 start_date = '1955-01-01'
 end_date = '2022-01-01'
-
+lambdas = [10, 100, 1600]
 # download the data from FRED using pandas_datareader
-country_id = 'NGDPRSAXDCBRQ' # Brazil, real GDP, quarterly
-gdp = web.DataReader(country_id, 'fred', start_date, end_date)
-log_gdp = np.log(gdp[country_id])
+country_series = {
+    "Brazil":'NGDPRSAXDCBRQ',
+    "Japan" :'NGDPRSAXDCJPQ',
+}
+
+# for loop of each country
+for country, series_id in country_series.items():
+    gdp = web.DataReader(series_id, 'fred', start_date, end_date)
+    log_gdp = np.log(gdp[series_id])
 
 # apply a Hodrick-Prescott filter to the data to extract the cyclical component
 # use for loop to compute every lambda
-lambdas = [10, 100, 1600]
-for lamb in lambdas:
-    cycle, trend = sm.tsa.filters.hpfilter(log_gdp, lamb)
+
+    for lamb in lambdas:
+        cycle, trend = sm.tsa.filters.hpfilter(log_gdp, lamb)
 
     # Plot the original time series data
-    plt.plot(log_gdp, label="Original GDP (in log)")
-    plt.title(f'Brazil: log GDP & HP-filter trend  (λ = {lamb})')
+        plt.figure(figsize=(10, 4))
+        plt.plot(log_gdp, label="Original GDP (in log)")
+        plt.title(f'{country}: log GDP & HP-filter trend  (λ = {lamb})')
 
     # Plot the trend component
-    plt.plot(trend, label="Trend")
+        plt.plot(trend, label="Trend")
 
     # Add a legend and show the plot
-    plt.legend()
-    plt.show()
+        plt.legend()
+        plt.show()
